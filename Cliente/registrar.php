1cf11caadbddb60b7fbcee3af41bb37fddb32857
ninocobo <79443182+ninocobo@users.noreply.github.com>
2022-09-14 23:37:36 +0200

    Elimino la longitud máxima de los input

--- Cliente/registrar.php
+++ Cliente/registrar.php
@@ -21,22 +21,22 @@
         <div class="form-row">
             <div class="form-group col-md-5">
                 <label for="nombre">Nombre</label>
-                <input type="text" class="form-control form-control-sm" id="nombre" name="nombre" placeholder="Introduce el nombre..." maxlength="100">
+                <input type="text" class="form-control form-control-sm" id="nombre" name="nombre" placeholder="Introduce el nombre..."> <!--maxlength='100'-->
             </div>
             <div class="form-group col-md-5">
                 <label for="apellido">Apellidos</label>
-                <input type="text" class="form-control form-control-sm" id="apellido" name="apellido" placeholder="Introduce los apellidos..." maxlength="100">
+                <input type="text" class="form-control form-control-sm" id="apellido" name="apellido" placeholder="Introduce los apellidos..."> <!--maxlength='100'-->
             </div>
         </div>
         <div class="form-row">
             <div class="form-group col-md-5">
                 <label for="dni">DNI</label>
-                <input type="text" class="form-control form-control-sm" id="dni" name="dni" placeholder="Introduce el DNI..." maxlength="20">
+                <input type="text" class="form-control form-control-sm" id="dni" name="dni" placeholder="Introduce el DNI..."> <!--maxlength='20'-->
             </div>
         </div>
         <button name="registrar" class="btn btn-primary">Registrar</button>
         <button type="reset" class="btn btn-primary" value="Borrar" id="borrar" name="borrar">Borrar datos</button>
-        <input type="button" class="btn btn-primary"onclick="location='contactos.php'" value="Volver"/>
+        <input type="button" class="btn btn-primary" onclick="location='contactos.php'" value="Volver"/>
     </form>
 </div>
 
@@ -46,24 +46,28 @@
             && isset($_REQUEST['apellido']) && !empty($_REQUEST['apellido'])
             && isset($_REQUEST['dni']) && !empty($_REQUEST['dni'])
           ){
-            $porData=array();
+            if ((strlen($_REQUEST['nombre']) > 100) || (strlen($_REQUEST['apellido']) > 100) || (strlen($_REQUEST['dni']) > 20)) {
+                echo "Error, longitud de los valores no permitida";
+            } else {
+                $porData=array();
 
-            if(validarDni($_REQUEST['dni'])){
-                $usuario=nuevoUsuario($_REQUEST['nombre'], $_REQUEST['apellido'], $_REQUEST['dni']);
-                $porData['nombre']=$usuario->getNombre();
-                $porData['apellido']=$usuario->getApellidos();
-                $porData['dni']=$usuario->getDni();
-                $ch = curl_init();
-                curl_setopt($ch, CURLOPT_URL, "localhost/api/Server/crearContacto");
-                curl_setopt($ch, CURLOPT_HEADER, false);
-                curl_setopt($ch, CURLOPT_POST, true);
-                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($porData));
-                curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-                $data = curl_exec($ch);
-                print_r($data);//mensaje de información
-                curl_close($ch);
-            }else{
-                echo "El dni no es válido";
+                if(validarDni($_REQUEST['dni'])){
+                    $usuario=nuevoUsuario($_REQUEST['nombre'], $_REQUEST['apellido'], $_REQUEST['dni']);
+                    $porData['nombre']=$usuario->getNombre();
+                    $porData['apellido']=$usuario->getApellidos();
+                    $porData['dni']=$usuario->getDni();
+                    $ch = curl_init();
+                    curl_setopt($ch, CURLOPT_URL, "localhost/api/Server/crearContacto");
+                    curl_setopt($ch, CURLOPT_HEADER, false);
+                    curl_setopt($ch, CURLOPT_POST, true);
+                    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($porData));
+                    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                    $data = curl_exec($ch);
+                    print_r($data);//mensaje de información
+                    curl_close($ch);
+                }else{
+                    echo "El dni no es válido";
+                }
             }
         }else{
             echo "Error, rellena todos los datos";
